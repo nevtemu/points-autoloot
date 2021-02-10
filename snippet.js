d07e596c4bf1cb34e692cecbb3ea1c3a40371402
nevtemu <artem.soroka@hotmail.com>
2021-02-10 10:04:50 +0400

    Added longer delay if redeemed successfully

--- snippet.js
+++ snippet.js
@@ -1,5 +1,11 @@
-function redeem () {let now = new Date()
-    if( document.querySelector('.tw-button--success')){document.querySelector('.tw-button--success').click();}
-    console.log(document.querySelector('.tw-button--success') !== null, `${now.getHours()}:${now.getMinutes()}` );
-    setTimeout(function(){ redeem(); }, 60000);}
+function redeem () {
+    let now = new Date()
+    if( document.querySelector('.tw-button--success')){
+        document.querySelector('.tw-button--success').click();
+        console.log(`Redeemed at ${now.getHours().length >1 ? now.getHours() : "0"+now.getHours()}:${now.getMinutes().length >1 ? now.getMinutes() : "0"+now.getMinutes()}`);
+        setTimeout(function(){ redeem(); }, 910000);}
+    else {
+        setTimeout(function(){ redeem(); }, 60000)
+    }
+}
 redeem();
