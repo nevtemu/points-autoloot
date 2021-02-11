21c9fd3dc228963ba2e9d84081c79a137efa4c10
nevtemu <artem.soroka@hotmail.com>
2021-02-11 08:53:01 +0400

    Fixed time format

--- snippet.js
+++ snippet.js
@@ -2,7 +2,7 @@ function redeem () {
     let now = new Date()
     if( document.querySelector('.tw-button--success')){
         document.querySelector('.tw-button--success').click();
-        console.log(`Redeemed at ${now.getHours().length >1 ? now.getHours() : "0"+now.getHours()}:${now.getMinutes().length >1 ? now.getMinutes() : "0"+now.getMinutes()}`);
+        console.log(`Redeemed at ${now.getHours() >9 ? now.getHours() : "0"+now.getHours()}:${now.getMinutes() >9 ? now.getMinutes() : "0"+now.getMinutes()}`);
         setTimeout(function(){ redeem(); }, 910000);}
     else {
         setTimeout(function(){ redeem(); }, 60000)
